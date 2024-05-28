f17636541ab47b633112b4cc3ab79c550ff521c2
alsahas <168373030+alsahas@users.noreply.github.com>
2024-05-28 17:42:56 +0300

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,17 +1,17 @@
 const categories=
 [
   { name: 'cat3'},
-  { name: 'mawad'},
+  { name: '„Ê«œ'},
   { name: 'skaker'},
-  { name: 'cat1'},
+  { name: 'cat1'}
 ];
 
 const products= 
 [
   { name: 'product1',category:'skaker',price: 11000 },
-  { name: 'product2',category:'mawad', price: 15000 },
+  { name: 'product2',category:'„Ê«œ', price: 15000 },
   { name: 'product3',category:'skaker', price: 20000 },
-  { name: 'product4',category:'cat3', price: 25000 },
+  { name: 'product4',category:'cat3', price: 2500000 },
   { name: 'product5',category:'cat3', price: 30000 },
   { name: 'product6',category:'cat3', price: 35000 },
   { name: 'product6',category:'cat3', price: 35000 },
@@ -23,8 +23,26 @@ const products=
   { name: 'product6',category:'cat3', price: 35000 },
   { name: 'product6',category:'cat3', price: 35000 },
   { name: 'product6',category:'cat3', price: 35000 },
-  { name: 'product6',category:'cat3', price: 350000 },
-  { name: 'product6',category:'cat3', price: 3500000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
+  { name: 'product6',category:'cat3', price: 35000 },
   { name: 'product6',category:'cat3', price: 35000 },
   { name: 'product6',category:'cat3', price: 35000 },
   { name: 'product6',category:'cat3', price: 35000 },
@@ -57,8 +75,33 @@ const products=
   { name: 'product7',category:'cat1', price: 40000 },
   { name: 'product8',category:'cat1', price: 45000 }
 ];
-const mycart =[];
-
+const mycart =
+[
+  { name: 'product7',quantity:1, price: 40000 },
+  { name: 'product8',quantity:2, price: 45000 }
+];
+function plus(number)
+{
+	alert(number);
+}
+function minus(number)
+{
+	alert(number);
+}
+function quantity(num,q)
+{
+	var prod=document.getElementById(num);
+	prod.innerHTML=q;
+	
+}
+function quantity(prodname)
+{
+  let quantity = prompt("Please enter quantity to product:"+prodname, document.getElementById(prodname).innerHTML);
+  if (quantity != null) 
+  {
+    document.getElementById(prodname).innerHTML =quantity;
+  }
+}
 function distributeCategories()
 {
 	var topnav=document.getElementById("myCategories");
@@ -77,32 +120,55 @@ function distributeMyCart()
 {
 	var page=document.getElementById("page");
 	page.innerHTML="";
-	const sidebars=document.querySelectorAll("#sidebar a[href]");
+	const sidebars=document.querySelectorAll("#topnav a[href]");
 	for(let j=0;j<sidebars.length;j++)sidebars[j].className="";
 	document.getElementById('My CART').className="active";
-	
-	for (let i = 0; i < mycart.length; i++) 
+	if(mycart.length==0)
 	{
-		const product = mycart[i];
-		
 		var div= document.createElement("div");
 		div.className="container";
 		
-		var img = document.createElement("img");
-		img.className="image";
-		img.src = "png/"+product.name+".png";
-		
 		var p=document.createElement("p");
-		p.className="overlay";
-		p.innerHTML=""+product.price;
+		p.innerHTML="YOUR CART IS EMPTY PLEASE SELECT PRODUCTS";
 		
-		div.append(img);
 		div.append(p);
 		
 		page.append(div);
 	}
+	else
+	{
+		for (let i = 0; i < mycart.length; i++) 
+		{
+			const product = products[i];
+			
+			var div= document.createElement("div");
+			div.className="container";
+			var a=document.createElement("a");
+			a.href="javascript:adProductToCart('"+product.name+"');";
+			var img = document.createElement("img");
+			img.className="image";
+			img.src = "png/"+product.name+".png";
+			img.width="70";
+			img.height="70";
+			var p=document.createElement("p");
+			p.className="overlay";
+			p.style="font-size:1rem;";
+			p.innerHTML=numberComma(product.price)+" L.L.";
+			
+			a.append(img)
+			div.append(a);
+			div.append(p);
+			
+			page.append(div);
+		}
+	}
 }
-
+function addProductToCart(number)
+{
+//	alert(""+number);
+	var b=document.getElementById("total");
+	b.innerHTML="50,000";
+}
 function distribute(cat)
 {
 	var page=document.getElementById("page");
@@ -117,9 +183,18 @@ function distribute(cat)
 		
 		if(cat==product.category)
 		{
+		/*simulation	
+		<div id="page" class="page">
+			<div class="container">
+				<a href="javascript:addProductToCart('productname');"><img class="image" src="png/productname.png" width=70 height=70>
+				<p class="overlay" style="font-size:1rem;">numberComma(product.price)+" L.L."</p>
+			</div>
+		</div>
+		*/
 			var div= document.createElement("div");
 			div.className="container";
-			
+			var a=document.createElement("a");
+			a.href="javascript:addProductToCart('"+product.name+"');";
 			var img = document.createElement("img");
 			img.className="image";
 			img.src = "png/"+product.name+".png";
@@ -127,10 +202,11 @@ function distribute(cat)
 			img.height="70";
 			var p=document.createElement("p");
 			p.className="overlay";
-			p.style="font-size: 30px;";
-			p.innerHTML=numberComma(product.price);
+			p.style="font-size:1rem;";
+			p.innerHTML=numberComma(product.price)+" L.L.";
 			
-			div.append(img);
+			a.append(img)
+			div.append(a);
 			div.append(p);
 			
 			page.append(div);
