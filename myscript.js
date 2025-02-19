a9519e096e4ea95440bfad0b63d5c38425d85a19
alsahas <168373030+alsahas@users.noreply.github.com>
2025-02-19 04:09:00 +0200

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -289,7 +289,11 @@ function distributeAll()
 }
 function distributeCategories()
 {
-	if((document.documentElement.clientWidth/document.documentElement.clientHeight)<0.62)screen.orientation.lock('landscape').catch((e) => alert(e));
+	if((document.documentElement.clientWidth/document.documentElement.clientHeight)<0.62)
+	{
+		document.documentElement.requestFullscreen();
+		screen.orientation.lock('landscape').catch((e) => alert(e));
+	}
 	alert(document.documentElement.clientWidth/document.documentElement.clientHeight);
 	var topnav=document.getElementById("myCategories");
 	for (let i = 0; i < categories.length; i++)
